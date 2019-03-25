afba8561c57a37ba08e58ae50655f5639301dff8
jhonatadev <xxxonata@gmail.com>
2019-03-25 15:33:21 -0300

    Trabalhando com variáveis [coment code]

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,5 @@
+// Trabalhando com variáveis
+
 let email = 'devupacademy@gmail.com';
 // const email = 'devupacademy@gmail.com';
 
@@ -6,6 +8,3 @@ email = 'jhonatadev@tuta.io';
 console.log(email);
 console.log('O seu e-mail é: ' + email);
 console.log(`O seu e-mail é ${email}`);
-
-
-
