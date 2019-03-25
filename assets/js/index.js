f3f5feb7c84c31df2227512a1303f8be5c0e267b
jhonatadev <xxxonata@gmail.com>
2019-03-25 17:47:06 -0300

    event js final, e um pouco de ajax

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,3 +5,12 @@ document.getElementById('btn-submit').addEventListener('click', e => {
 document.getElementById('form-login').addEventListener('mouseenter', e => {
     console.log('O mouse passou sobre o formulario');
 });
+
+document.querySelector('#form-login').addEventListener('mouseleave', e => {
+    console.log('O mouse está fora do formulario');
+});
+
+document.querySelector('#form-login').addEventListener('submit', e => {
+    e.preventDefault();
+    console.log('Formulário enviado! Aqui vai o Ajax');
+});
